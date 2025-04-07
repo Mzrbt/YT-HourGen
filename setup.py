069d512ce2a0cbac24a92f0c2994c90408c68d98
Mzrbt <jabetmarin07@gmail.com>
2025-04-07 15:30:06 +0200

    Mod setup.py & Mod looper.py + Enable command yt-hourgen

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from setuptools import setup, find_packages
 setup(
     name='YT-HourGen',  # Le nom de ton projet
     version='0.1',  # Version initiale
-    packages=find_packages(),  # Pour inclure tous les packages dans le répertoire
+    packages=['yt_hourgen'],  # Pour inclure tous les packages dans le répertoire
     install_requires=[  # Les dépendances nécessaires
         'pytube',  # Exemple, tu peux ajouter ici toutes les dépendances listées dans requirements.txt
         'moviepy',
     ],
     entry_points={  # Permet de définir une commande dans le terminal
         'console_scripts': [
-            'yt-hourgen=main:main',  # Cette ligne permet d'exécuter `yt-hourgen` depuis le terminal
+            'yt-hourgen=yt_hourgen.main:main',  # Cette ligne permet d'exécuter `yt-hourgen` depuis le terminal
         ],
     },
     include_package_data=True,  # Permet d'inclure les fichiers supplémentaires (comme assets)
---
+++ yt_hourgen/looper.py
@@ -0,0 +1,3 @@
+def make_video_one_hour():
+    print("La vidéo d'une heure est en cours de création.")
+    # Code pour générer la vidéo d'une heure ici
