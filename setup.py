3223e09e3f09f05d5d64baff158af8067fe0dced
Mzrbt <jabetmarin07@gmail.com>
2026-06-04 18:47:47 +0200

    [FIX] Version upgrade in setup.py & mod .gitignore

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='yt-hourgen',
-    version='1.0.0',
+    version='1.1.0',
     author='Mzrbt',
     description='Generate 1-hour looped videos from YouTube',
     long_description=long_description,
